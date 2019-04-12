e756df36c73c0ce5d1b96801157498a443f0f847
lutery <lutery@foxmail.com>
2019-04-12 23:32:34 +0800

    初始化提交

---
+++ src/autocardpatch/src/main/java/one/chchy/autocardpatch/AutocardpatchApplication.java
@@ -0,0 +1,13 @@
+package one.chchy.autocardpatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutocardpatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutocardpatchApplication.class, args);
+    }
+
+}
---
+++ src/autocardpatch/src/test/java/one/chchy/autocardpatch/AutocardpatchApplicationTests.java
@@ -0,0 +1,16 @@
+package one.chchy.autocardpatch;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AutocardpatchApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
